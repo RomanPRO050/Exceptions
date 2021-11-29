c73ece567529c2625dd8198943e3d22630a7143f
RomanPRO050 <boev.rs@gmail.com>
2021-11-29 22:49:36 +0300

    add raise

--- probe.py
+++ probe.py
@@ -1,6 +1,5 @@
-
 def calc(line):
-    #print(f'Read line {line}', flush=True)
+    # print(f'Read line {line}', flush=True)
     operand_1, operation, operand_2 = line.split(' ')
     operand_1 = int(operand_1)
     operand_2 = int(operand_2)
@@ -18,8 +17,10 @@ def calc(line):
         value = operand_1 % operand_2
 
     else:
-        print(f'Unknown operand {operation}')
+        raise ValueError(f'Unknown operation {operation}')
     return value
+
+
 total = 0
 
 with open('calc.txt', 'r') as ff:
